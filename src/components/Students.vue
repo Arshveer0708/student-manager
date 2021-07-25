a926265b37b049be97d58be059a09bee69e36766
Arshveer0708 <64243859+Arshveer0708@users.noreply.github.com>
2021-07-25 07:19:00 -0400

    Using getters

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,14 +1,27 @@
 <template>
- <v-flex sm8 offset-sm2>
-   {{students}}
+  <v-flex sm8 offset-sm2>
     <v-card>
       <v-toolbar dark>
         <v-toolbar-title>Students</v-toolbar-title>
       </v-toolbar>
-      <v-list>
-        <v-list-tile v-for="(student, index) in students" :to="'editStudent/' + index">
+
+      <v-container class="text-xs-center">
+        <v-progress-circular
+          v-if="!$store.getters.isLoaded"
+          :size="70"
+          :width="7"
+          color="purple"
+          indeterminate
+        ></v-progress-circular>
+      </v-container>
+
+      <v-list v-if="$store.getters.isLoaded">
+        <v-list-tile
+          v-for="(student, index) in $store.getters.students"
+          :to="'editStudent/' + index"
+        >
           <v-list-tile-content>
-            <v-list-tile-title v-text="student.name"></v-list-tile-title>
+            <v-list-tile-title v-text="student.fullName"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -21,14 +34,12 @@ import axios from "axios";
 import { mapState } from "vuex";
 
 export default {
-    data() {
-        return {
-        }
-    },
-    computed: mapState({
-      students: state => state.students
-    }),
-  async created() {
-  }
+  data() {
+    return {};
+  },
+  computed: mapState({
+    students: state => state.students
+  }),
+  async created() {}
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,14 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state : {
+    state: {
         students: []
+    },
+    getters: {
+        students: state => state.students.map(s => ({
+            ...s, fullName: s.firstName + ' ' + s.lastName
+        })),
+        findStudent: state => id => state.students.find(s => s.id == id),
+        isLoaded: state => !!state.students.length
     }
 })
